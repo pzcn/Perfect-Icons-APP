34f560f812aa728fc49c120b2f89f4dff9317cc3
PedroZ <sdustpedro@gmail.com>
2021-05-28 18:48:50 +0800

    [Release]修复note8pro等设备白屏问题

--- pio/src/main/assets/script/before_start.sh
+++ pio/src/main/assets/script/before_start.sh
@@ -1,7 +1,6 @@
 #!/system/bin/sh
 echo "APP需要连接网络才可以正常使用..."
-[ -f $TOOLKIT/curl ] && chmod -R 777 $TOOLKIT/curl
- command -v curl >/dev/null 2>&1 || ( echo "缺少curl命令，开始下载..." ; $TOOLKIT/wget -q "https://miuiicons-generic.pkg.coding.net/icons/files/curl?version=latest" -O $TOOLKIT/curl 2>/dev/null ; chmod -R 777 $TOOLKIT/curl )
+chmod -R 777 $TOOLKIT/curl
 echo "开始初始化..."
 curl -skLJo "$START_DIR/script/before_start_online.sh" https://miuiicons-generic.pkg.coding.net/icons/files/before_start.sh?version=latest
 chmod 755 "$START_DIR/script/before_start_online.sh"
