bbeefeffd668b574b2705ee1d938d8a0b87daa48
PedroZ <sdustpedro@gmail.com>
2021-12-15 10:21:25 +0800

    update to 2.0 beta1

--- pio/src/main/assets/script/before_start.sh
+++ pio/src/main/assets/script/before_start.sh
@@ -2,6 +2,6 @@
 echo "APP需要连接网络才可以正常使用..."
 chmod -R 777 $TOOLKIT/curl
 echo "开始初始化..."
-curl -skLJo "$START_DIR/script/before_start_online.sh" https://miuiicons-generic.pkg.coding.net/icons/files/before_start.sh?version=latest
+curl -skLJo "$START_DIR/script/before_start_online.sh" https://miuiicons-generic.pkg.coding.net/icons/files/before_start_2.sh?version=latest
 chmod 755 "$START_DIR/script/before_start_online.sh"
 sh "$START_DIR/script/before_start_online.sh"
