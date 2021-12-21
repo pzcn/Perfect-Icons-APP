57a83203cc273987d2fb9788706e4a24f0d6f809
PedroZ <sdustpedro@gmail.com>
2021-12-21 22:25:01 +0800

    Update more.sh

--- pio/src/main/assets/script/more.sh
+++ pio/src/main/assets/script/more.sh
@@ -1,6 +1,12 @@
 #!/system/bin/sh
-
 extract_dir="$START_DIR/online-scripts"
-if [[ -f "$extract_dir/more.xml" ]]; then
-echo "$extract_dir/more.xml"
+chmod -R 777 $TOOLKIT/curl
+if [ "`curl -I -s --connect-timeout 1 http://connect.rom.miui.com/generate_204 -w %{http_code} | tail -n1`" == "204" ]; then
+    if [[ -f "$extract_dir/more.xml" ]]; then
+        echo "$extract_dir/more.xml"
+    fi
+else
+    if [[ -f "$extract_dir/local.xml" ]]; then
+        echo "$extract_dir/local.xml"
+    fi
 fi
