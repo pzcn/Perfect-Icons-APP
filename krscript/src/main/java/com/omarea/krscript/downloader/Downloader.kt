54d2fc26b22fdd3fed6d4ef67550a4b7d09a8661
pzcn <sdustpedro@gmail.com>
2021-12-21 10:20:51 +0800

    Update Downloader.kt

--- krscript/src/main/java/com/omarea/krscript/downloader/Downloader.kt
+++ krscript/src/main/java/com/omarea/krscript/downloader/Downloader.kt
@@ -40,7 +40,7 @@ class Downloader(private var context: Context, private var activity: Activity? =
             // 允许媒体扫描，根据下载的文件类型被加入相册、音乐等媒体库
             request.allowScanningByMediaScanner()
             // 设置通知的显示类型，下载进行时和完成后显示通知
-            request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED)
+            request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE)
             // 设置通知栏的标题，如果不设置，默认使用文件名
             //        request.setTitle("This is title");
             // 设置通知栏的描述
@@ -48,7 +48,7 @@ class Downloader(private var context: Context, private var activity: Activity? =
             // 允许在计费流量下下载
             request.setAllowedOverMetered(true)
             // 允许该记录在下载管理界面可见
-            request.setVisibleInDownloadsUi(true)
+            request.setVisibleInDownloadsUi(false)
             // 允许漫游时下载
             request.setAllowedOverRoaming(true)
             // 允许下载的网路类型
@@ -56,7 +56,7 @@ class Downloader(private var context: Context, private var activity: Activity? =
             // request.setAllowedNetworkTypes(DownloadManager.Request.NETWORK_MOBILE);
             // 设置下载文件保存的路径和文件名
             val outName = if(fileName.isNullOrEmpty()) URLUtil.guessFileName(url, contentDisposition, mimeType) else fileName
-            request.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, outName)
+            request.setDestinationInExternalFilesDir(Environment.DIRECTORY_DOWNLOADS, outName)
             //        另外可选一下方法，自定义下载路径
             //        request.setDestinationUri()
             //        request.setDestinationInExternalFilesDir()
