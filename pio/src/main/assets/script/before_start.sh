c670a57a5b95c7b99a59a10743436a70aa926978
PedroZ <sdustpedro@gmail.com>
2021-12-21 22:51:12 +0800

    2

--- pio/src/main/assets/script/before_start.sh
+++ pio/src/main/assets/script/before_start.sh
@@ -1,7 +1,6 @@
 #!/system/bin/sh
 chmod -R 777 $TOOLKIT/curl
 echo "开始初始化..."
-extract_dir="$START_DIR/online-scripts"
 addon_path=/sdcard/Documents/MIUI完美图标自定义
 [ -d "theme_files" ] || mkdir theme_files
 [ -f "theme_files/theme_config" ] || ( touch theme_files/theme_config && echo "sel_theme=default" > theme_files/theme_config )
@@ -12,7 +11,23 @@ addon_path=/sdcard/Documents/MIUI完美图标自定义
 [ -f "theme_files/hwt_shape_config" ] || ( touch theme_files/hwt_shape_config && echo "hwt_shape=Rectangle" > theme_files/hwt_shape_config )
 [ -f "theme_files/update_status.ini" ] && ( rm -rf theme_files/update_status.ini )
 [ -f "theme_files/download_config" ] || ( touch theme_files/download_config && echo "curlmode=0" > theme_files/download_config )
+source theme_files/beta_config
 
-if [ "`curl -I -s --connect-timeout 1 http://connect.rom.miui.com/generate_204 -w %{http_code} | tail -n1`" == "204" ]; then
-   curl -skLJo "$extract_dir/misc/announce.txt" "https://miuiicons-generic.pkg.coding.net/icons/files/announce.txt?version=latest"
+if [[ beta = 1 ]]; then
+   extract_dir="$START_DIR/online-scripts"
+   echo "当前为Beta通道..."
+   [ -d "$START_DIR/online-scripts" ] || mkdir -p $START_DIR/online-scripts
+   if [ "`curl -I -s --connect-timeout 1 http://connect.rom.miui.com/generate_204 -w %{http_code} | tail -n1`" == "204" ]; then
+      curl -skLJo "$START_DIR/online-scripts/before_start.sh" https://miuiicons-generic.pkg.coding.net/icons/files/before_start_23.sh?version=latest
+      chmod 755 "$START_DIR/online-scripts/before_start.sh"
+      sh "$START_DIR/online-scripts/before_start.sh"
+   fi
+else
+   extract_dir="$START_DIR/local-scripts"
+   if [ "`curl -I -s --connect-timeout 1 http://connect.rom.miui.com/generate_204 -w %{http_code} | tail -n1`" == "204" ]; then
+      curl -skLJo "$extract_dir/misc/announce.txt" "https://miuiicons-generic.pkg.coding.net/icons/files/announce.txt?version=latest"
+   fi
 fi
+
+
+
--- pio/src/main/assets/script/more.sh
+++ pio/src/main/assets/script/more.sh
@@ -1,5 +1,4 @@
 #!/system/bin/sh
-extract_dir="$START_DIR/online-scripts"
 chmod -R 777 $TOOLKIT/curl
 if [ "`curl -I -s --connect-timeout 1 http://connect.rom.miui.com/generate_204 -w %{http_code} | tail -n1`" == "204" ]; then
     if [[ -f "$extract_dir/more.xml" ]]; then
