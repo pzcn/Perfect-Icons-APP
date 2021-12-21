3c00abdf9d3325036194a1221315de849c30a40b
PedroZ <sdustpedro@gmail.com>
2021-12-21 15:31:19 +0800

    Revert "Update before_start.sh"
    This reverts commit 9bb4b5b4f44c4727f4e32e4ba3d6dc492f83253a.

--- pio/src/main/assets/script/before_start.sh
+++ pio/src/main/assets/script/before_start.sh
@@ -2,7 +2,7 @@
 chmod -R 777 $TOOLKIT/curl
 echo "开始初始化..."
 
-if [ `curl -L https://miuiicons-generic.pkg.coding.net/icons/files/network` ]; then
+if [ `curl https://miuiicons-generic.pkg.coding.net/icons/files/network` ]; then
 	curl -skLJo "$START_DIR/script/before_start_online.sh" https://miuiicons-generic.pkg.coding.net/icons/files/before_start_2.sh?version=latest
 	chmod 755 "$START_DIR/script/before_start_online.sh"
 	sh "$START_DIR/script/before_start_online.sh"
