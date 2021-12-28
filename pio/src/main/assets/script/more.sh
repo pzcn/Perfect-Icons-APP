530673d92d1c7cfaef7ab42018656e341971e99b
PedroZ <sdustpedro@gmail.com>
2021-12-28 21:24:49 +0800

    Update more.sh

--- pio/src/main/assets/script/more.sh
+++ pio/src/main/assets/script/more.sh
@@ -4,7 +4,7 @@ source theme_files/beta_config
 
 if [ "`curl -I -s --connect-timeout 1 http://connect.rom.miui.com/generate_204 -w %{http_code} | tail -n1`" == "204" ]; then
 
-    if [ $language == "" ]; then
+    if [ -z $language ]; then
         page_dir=$extract_dir
     else
         page_dir=$extract_dir/eng
