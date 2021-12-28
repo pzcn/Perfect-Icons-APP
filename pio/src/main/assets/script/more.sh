509798eba92b4ea409008440c1936e5d078a7d8b
PedroZ <sdustpedro@gmail.com>
2021-12-28 20:58:48 +0800

    Update more.sh

--- pio/src/main/assets/script/more.sh
+++ pio/src/main/assets/script/more.sh
@@ -3,16 +3,13 @@ chmod -R 777 $TOOLKIT/curl
 source theme_files/beta_config
 
 if [ "`curl -I -s --connect-timeout 1 http://connect.rom.miui.com/generate_204 -w %{http_code} | tail -n1`" == "204" ]; then
-    if [[ $beta = 1 ]]; then
-        extract_dir="$START_DIR/online-scripts"
-    else
-        extract_dir="$START_DIR/local-scripts"
-    fi
-    if [ "$(getprop persist.sys.locale)" = "zh-CN" ]; then
+
+    if [ $language == "" ]; then
         page_dir=$extract_dir
     else
         page_dir=$extract_dir/eng
     fi
+    
     if [[ -f "$page_dir/more.xml" ]]; then
         echo "$page_dir/more.xml"
     fi
