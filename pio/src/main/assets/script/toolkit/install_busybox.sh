ae588e4ff09b17a724501c41e4bc8a43d7bbbe95
YifePlayte <yife-playte@foxmail.com>
2021-12-31 22:37:39 +0800

    release

--- pio/src/main/assets/script/toolkit/install_busybox.sh
+++ pio/src/main/assets/script/toolkit/install_busybox.sh
@@ -1,9 +1,12 @@
 if [ -e "$TOOLKIT/sh" ];then
 exit
 else
-echo "安装busybox..."
+echo "Installing busybox..."
 busybox="$TOOLKIT/busybox1"
 ln -sf $busybox $TOOLKIT/busybox
 $busybox --install -s $TOOLKIT
 chmod -R 777 $TOOLKIT
+fi
+if [ ! -e "$TOOLKIT/git-remote-https" ];then
+	ln -sf $TOOLKIT/git-remote-http $TOOLKIT/git-remote-https
 fi
