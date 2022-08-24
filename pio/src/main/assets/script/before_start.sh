83c7886c7d56b1d7427820dea0a8390bcf4b2482
pzcn <sdustpedro@gmail.com>
2022-08-24 23:15:51 +0800

    Update before_start.sh

--- pio/src/main/assets/script/before_start.sh
+++ pio/src/main/assets/script/before_start.sh
@@ -37,6 +37,6 @@ if [[ $beta = 1 ]]; then
    fi
 else
    if [ "`curl -I -s --connect-timeout 1 http://connect.rom.miui.com/generate_204 -w %{http_code} | tail -n1`" == "204" ]; then
-      curl -skLJo "theme_files/announce.txt" "https://miuiicons-generic.pkg.coding.net/icons/files/announce252${language}.txt?version=latest"
+      curl -skLJo "theme_files/announce.txt" "https://miuiicons-generic.pkg.coding.net/icons/files/announce273${language}.txt?version=latest"
    fi
 fi
