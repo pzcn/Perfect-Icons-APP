de566b43b223a07ebfff49a3dbbf8c8cef6f19db
pzcn <sdustpedro@gmail.com>
2023-01-23 20:02:11 +0800

    update

--- pio/src/main/assets/script/toolkit/install_busybox.sh
+++ pio/src/main/assets/script/toolkit/install_busybox.sh
@@ -1,10 +1,25 @@
-if [ ! -e "$TOOLKIT/sh" ];then
-echo "Installing busybox..."
-busybox="$TOOLKIT/busybox1"
-ln -sf $busybox $TOOLKIT/busybox
-$busybox --install -s $TOOLKIT
-chmod -R 777 $TOOLKIT
+version=20230123
+busybox_install() {
+	$TOOLKIT/busybox --install -s $TOOLKIT
+	echo $version >>$TOOLKIT/busybox.version
+	chmod -R 777 $TOOLKIT
+}
+
+if [ ! -e "$TOOLKIT/sh" ]; then
+	echo "Installing busybox..."
+	busybox_install
 fi
-if [ ! -e "$TOOLKIT/git-remote-https" ];then
+
+if [ -f $TOOLKIT/busybox.version ]; then
+	if [ "$version" -gt $(cat "$TOOLKIT/busybox.version") ]; then
+		echo "Updating busybox..."
+		busybox_install
+	fi
+else
+	echo "Updating busybox..."
+	busybox_install
+fi
+
+if [ ! -e "$TOOLKIT/git-remote-https" ]; then
 	ln -sf $TOOLKIT/git-remote-http $TOOLKIT/git-remote-https
 fi
