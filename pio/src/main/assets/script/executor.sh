0eaf268fbe99ea042c0ee90bd2aa1ee66179b5c6
pzcn <sdustpedro@gmail.com>
2023-01-24 19:11:53 +0800

    清理缓存

--- pio/src/main/assets/script/executor.sh
+++ pio/src/main/assets/script/executor.sh
@@ -80,3 +80,14 @@ if [[ -f "$script_path" ]]; then
 else
     echo "${script_path} 已丢失" 1>&2
 fi
+
+# 清理
+if [[ -d "${START_DIR}/downloader" ]]; then
+  rm -rf ${START_DIR}/downloader/*
+fi
+if [[ -d "${START_DIR}/kr-script" ]]; then
+  rm -rf ${START_DIR}/kr-script/*
+fi
+if [[ -d "${TEMP_DIR}" ]]; then
+  rm -rf ${TEMP_DIR}/*
+fi
