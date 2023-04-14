f40f18da6c827efeb516bf1f7c6ab65df5abd74f
柚稚的孩纸 <30484319+zjw2017@users.noreply.github.com>
2023-04-14 15:11:06 +0800

    format code

--- pio/src/main/assets/local-scripts/misc/module.sh
+++ pio/src/main/assets/local-scripts/misc/module.sh
@@ -38,8 +38,8 @@ EOF
 
   cat >$TEMP_DIR/moduletmp/post-fs-data.sh <<'EOF'
 MODDIR="${0%/*}"
-for i in /data/user/0/com.xiaomi.market/files/download_icon /data/user/0/com.xiaomi.market/cache/icons;do
-  if [ -d $i ];then
+for i in /data/user/0/com.xiaomi.market/files/download_icon /data/user/0/com.xiaomi.market/cache/icons; do
+  if [ -d $i ]; then
     rm -rf $i/*
     chattr +i $i
   fi
